e9f8cfa226554524f6c922ea4edaa706a1dc3a4e
vipul <40529003+vipulsnk@users.noreply.github.com>
2020-09-22 17:09:03 +0530

    adding testcases

--- ass1_q4/src/UnitTestCases.java
+++ ass1_q4/src/UnitTestCases.java
@@ -41,3 +41,21 @@ void testcase3() {
         }
     }
 }
+
+void testcase4() {
+    int cachePower = 18; // cache size = 2^18B
+    int blockPower = 5; // block size = 2^6B
+    int N = 512;
+    int[][] A = new int[N][N];
+    int[][] B = new int[N][N];
+    int[][] C = new int[N][N];
+    String cacheType = "DirectMapped";
+    for (int i = 0; i < N; i += 1) {
+        for (int k = 0; k < N; k += 1) {
+            int sum = 0;
+            for (int j = 0; j < N; j += 1) {
+            	C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
